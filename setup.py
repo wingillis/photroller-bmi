da57f039240cd37aa1ec2f0fd264518511eafe19
Winthrop Gillis <win.gillis@gmail.com>
2021-02-13 14:48:54 -0500

    remove matplotlib

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
     description='To boldly go where no mouse has gone before',
     version='0.002a',
     platforms=['mac', 'unix'],
-    install_requires=['h5py', 'matplotlib', 'scipy',
-                      'numpy', 'click', 'pyserial',
+    install_requires=['h5py', 'scipy', 'numpy', 'click', 'pyserial',
                       'ruamel.yaml', 'PySide6', 'pyqtgraph'],
     python_requires='>=3.6',
     entry_points={'console_scripts': ['photroller = photroller.cli:cli']}
