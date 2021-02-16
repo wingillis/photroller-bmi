b489c72db769039f60d617295e29c8cb9f9a8140
Winthrop Gillis <win.gillis@gmail.com>
2021-02-15 20:02:32 -0500

    better install

---
+++ readme.md
@@ -0,0 +1,6 @@
+
+Run this command in your terminal
+pip install git+https://github.com/pyqtgraph/pyqtgraph.git@pyqtgraph-0.11.1
+
+make sure your udev rules allow you to write to connect arduinos
+
--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+print('Run this command in your terminal')
+print('pip install git+https://github.com/pyqtgraph/pyqtgraph.git@master')
+
+print()
+print('make sure your udev rules allow you to write to connect arduinos')
+
 setup(
     name='photroller',
     author='Jeff Markowitz, Winthrop Gillis',
@@ -8,7 +14,7 @@ setup(
     platforms=['mac', 'unix'],
     install_requires=['h5py', 'scipy', 'numpy', 'click', 'pyserial',
                       'ruamel.yaml', 'PySide6', 'labjack-ljm',
-                      'git+https://github.com/pyqtgraph/pyqtgraph@master'],
+                      ],
     python_requires='>=3.6',
     entry_points={'console_scripts': ['photroller = photroller.cli:cli']}
 )
